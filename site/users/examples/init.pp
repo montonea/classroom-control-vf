a2fdb3cf779576369ba00b10d3ef3d8f16de7faa
montonea <anthony_montone@abtassoc.com>
2016-04-05 14:02:59 -0400

    Update init.pp
    Corrected a syntax error

--- site/users/examples/init.pp
+++ site/users/examples/init.pp
@@ -1,1 +1,7 @@
-include users
+class users {
+  user { 'fundamentals':
+    ensure  => present
+    # password  => 'puppet8#labs',# Windows requires a plain text password
+    # groups    => ['Users'],   # Display in Windows Control Panel
+    }
+  }
