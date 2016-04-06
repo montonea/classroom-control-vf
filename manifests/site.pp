690e4469a1addab1b4367ebee8c99eff9f263687
montonea <anthony_montone@abtassoc.com>
2016-04-06 14:24:26 -0400

    Update site.pp
    Syntax correction

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,7 +38,7 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-node 'montonea_production' {
+node 'montonea.puppetlabs.vm' {
   notify { 'test_message' :
     message => "Hello, my name is ${::hostname}",
   }
