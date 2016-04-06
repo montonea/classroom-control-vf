d89c805957db20681233b896c8c2e3ee0bfaba4b
montonea <anthony_montone@abtassoc.com>
2016-04-06 11:59:24 -0400

    Update site.pp
    corrected the site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,21 +38,15 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
+node 'montonea.puppetlabs.vm' {
+  notify { 'test_message' :
+    message => "Hello, my name is ${::hostname}",
+  }
+
+  # include users
+  # include skeleton
+}
+
 node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
-  notify { "Hello, my name is ${::hostname}": }
- 
-
-  # file {'/etc/motd':
-  # ensure  => file,
-  # owner   => 'root',
-  # group   => 'root',
-  # mode    => '0644',
-  # content => "Have a nice day!",
-#}
-
-include memcached
-
+  notify { "${::fqdn} has no node definition": }
 }
